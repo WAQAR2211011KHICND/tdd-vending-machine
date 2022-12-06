9c163654de91e1f8a755cba002031a2e1758712c
WAQAR2211011KHICND <mohammad.waqar@xloopdigital.com>
2022-12-06 16:56:07 +0500

    Step 01 Passed

--- src/Machine.js
+++ src/Machine.js
@@ -1,9 +1,11 @@
 module.exports = class Machine {
+    
     constructor() {
 
     }
 
 
     seeSelections() {
+        return [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
     }
 };
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,16 +1,36 @@
 const Machine = require('../src/Machine');
 
 describe('the vending machine', () => {
-    it('should have items to purchase', () => {
+
+   
+
+    /**
+     As a customer, I want to see that the vending machine has items, so that I can purchase them.
+    - Given that I approach the vending machine
+    - when I look at it,
+    - then I see items inside that I can buy
+    - seeSelections()` returns an array of objects with item and price:
+    - `[{'crisps': 'Rs 100'}, {'chocolate': 'Rs 350'}, {'mints': 'Rs 70'}]`
+     */
+    
+    it("Vending Machine will shown, that it has ",()=>{
         // setup
+
         const machine = new Machine();
         const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
 
-        // exercise
+
+        //exercise
         const actual = machine.seeSelections();
 
+
         // assert
-        expect(true).toEqual(true);
+        expect(expected).toEqual(actual);
+
     });
+
+
+    
+
 
 });
