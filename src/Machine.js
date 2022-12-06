2556e5d4b4336f568934a4752a0a3c7afdec183b
WAQAR2211011KHICND <mohammad.waqar@xloopdigital.com>
2022-12-06 17:47:13 +0500

    we have refactor the code Story 01

--- src/Machine.js
+++ src/Machine.js
@@ -1,11 +1,37 @@
 module.exports = class Machine {
     
+    credit = 0 ;
+    Items = [{code : 1, name :'crisps'   , RS:   100},
+             {code : 2, name :'chocolate', RS:   350}, 
+             {code : 3, name :'mints'    , RS:   70}
+            ];
+
     constructor() {
 
     }
 
 
     seeSelections() {
-        return [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
+       
+        let Item = [];
+        this.Items.forEach(i=>{
+            Item.push({name: i.name, RS: i.RS});
+        })
+
+        return Item;
+    }
+
+    deposit(credits){
+
+        if(credits === 10||credits === 20||credits === 50||credits === 100||credits === 500){
+            this.credit += credits; 
+            return  `You have deposited Rs ${this.credit}`;
+        }
+        else
+            return 'Invalid Credited Amount';
+    }
+
+    selectItem(codes){
+        return 'The item you selected is unavailable';
     }
 };
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -17,7 +17,9 @@ describe('the vending machine', () => {
         // setup
 
         const machine = new Machine();
-        const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
+        const expected = [{name: 'crisps', RS: 100}, 
+                         {name: 'chocolate', RS: 350},
+                         {name: 'mints', Rs: 70}];
 
 
         //exercise
@@ -29,8 +31,91 @@ describe('the vending machine', () => {
 
     });
 
+/**
+ * 
+
+As a customer, I want to know how much money I have deposited, so that I know what I can purchase.
+- Given I am using the vending machine, 
+- when I insert money, 
+- then I see the total I have deposited on a screen. 
+- `deposit(100)` returns `'You have deposited Rs 100'`
+- The machine should accept bills in these amounts: `10, 20, 50, 100, 500`
+ 
+ */
+    it("The machine should accept bills in these amounts: `10, 20, 50, 100, 500",()=>{
+    // setup
+
+    const machine = new Machine();
+    const money = 10;
+    const expected = `You have deposited Rs ${money}`;
+
+    //exercise
+    const actual = machine.deposit(money);
+
+
+    // assert
+    expect(expected).toEqual(actual);
+
+});
+
+
+/**
+ As a customer, I want to add additional money, so that I can use the denominations I have to purchase an item.
+- Given I have deposited money in the vending machine,
+- when I deposit additional money,
+- then I see the new total on a screen. 
+- After depositing Rs 100, `deposit(50)` returns `'You have deposited Rs 150'`
+
+ */
+it("The machine should accepted the additional money, in these amounts: `10, 20, 50, 100, 500",()=>{
+    // setup
+
+    const machine = new Machine();
+    const initialMoney = 10;
+    machine.deposit(initialMoney);
 
     
+    //exercise
+    
+        const extraMoney = 10;
+        const actual = machine.deposit(extraMoney);
+        
+        const expected = `You have deposited Rs ${20}`;
+
+
+    // assert
+    expect(expected).toEqual(actual);
+
+});
+
+/**
+ 
+As a customer, I want to see a message if my item is unavailable, so that I can make another choice.
+- Given I am using the vending machine, 
+- when I enter a code for an item that is unavailable, 
+- then I see a message that the item is unavailable.
+- `selectItem(code)` returns `'The item you selected is unavailable'`
+ */
+it("The machine should accepted the additional money, in these amounts: `10, 20, 50, 100, 500",()=>{
+    // setup
+
+    const machine = new Machine();
+    const initialMoney = 10;
+    machine.deposit(initialMoney);
+
+    
+    //exercise
+    
+        const extraMoney = 10;
+        const actual = machine.deposit(extraMoney);
+        
+        const expected = `You have deposited Rs ${20}`;
+
+
+    // assert
+    expect(expected).toEqual(actual);
+
+});
 
 
 });
